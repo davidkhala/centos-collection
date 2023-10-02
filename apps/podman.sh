8506ae4bc333d38335093f83a25c595b9d001975
David Liu <david-khala@hotmail.com>
2023-10-02 18:29:54 +0800

    Update podman.sh

--- apps/podman.sh
+++ apps/podman.sh
@@ -9,10 +9,7 @@ asDocker() {
 start() {
   systemctl --user enable --now podman.socket
 }
-desktop-preinstall(){
-  # To resolve issue https://github.com/flatpak/flatpak/issues/4724
-  sudo flatpak remote-add --if-not-exists --system flathub https://flathub.org/repo/flathub.flatpakrepo
-}
+
 socket() {
   
   echo /run/user/${UID}/podman/podman.sock
