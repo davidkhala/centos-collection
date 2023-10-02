f162f5a72bc50866fe1129b9ef95997de314fdf7
David Liu <david-khala@hotmail.com>
2023-10-02 17:20:50 +0800

    Create install.sh

---
+++ apps/k8s/install.sh
@@ -0,0 +1,17 @@
+add-repo(){
+  cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/repodata/repomd.xml.key
+exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
+EOF
+
+}
+kube3(){
+  sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+  sudo systemctl enable --now kubelet
+
+}
