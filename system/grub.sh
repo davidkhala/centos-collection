56ded90f554855178da986189e565a10a542a650
David Liu <david-khala@hotmail.com>
2023-11-15 16:00:06 +0800

    Update grub.sh

--- system/grub.sh
+++ system/grub.sh
@@ -4,6 +4,11 @@ reload(){
 edit(){
   sudo vi /etc/default/grub
 }
+install-grub-customizer(){
+  # The GUI tools
+  sudo dnf install -y https://rpmfind.net/linux/epel/9/Everything/x86_64/Packages/g/grub-customizer-5.2.2-2.el9.x86_64.rpm
+
+}
 
 $@
 
