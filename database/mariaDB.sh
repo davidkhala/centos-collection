e92ec2fb48bbfd28ff2f8b62b554d37acbceab6f
David Liu <david-khala@hotmail.com>
2023-11-29 20:38:28 +0800

    Update mariaDB.sh

--- database/mariaDB.sh
+++ database/mariaDB.sh
@@ -2,6 +2,7 @@ set -e
 install() {
   # According to https://docs.rackspace.com/support/how-to/installing-mariadb-server-on-centos/
   sudo dnf install -y mariadb-server
+  curl https://raw.githubusercontent.com/davidkhala/centos-collection/main/database/mysql.sh | bash -s firewall
   curl https://raw.githubusercontent.com/davidkhala/linux-utils/main/database/mariaDB.sh | bash -s post-install
 
 }
