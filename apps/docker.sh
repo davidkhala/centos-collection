aed14373a88fc8f1607c5051a660bee35f35915d
David Liu <david-khala@hotmail.com>
2023-12-26 21:50:57 +0800

    Update docker.sh

--- apps/docker.sh
+++ apps/docker.sh
@@ -4,4 +4,10 @@ install-rootless(){
   curl https://raw.githubusercontent.com/davidkhala/linux-utils/main/apps/docker.sh | bash -s install-rootless
 
 }
+install-compose(){
+  sudo yum install -y yum-utils
+  sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+  sudo yum update
+  sudo yum install docker-compose-plugin
+}
 $@
