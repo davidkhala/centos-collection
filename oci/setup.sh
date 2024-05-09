d0fdbe5ba36838c1f24c2306b465bf0377d7d6e2
David Liu <david-khala@hotmail.com>
2024-05-09 16:16:13 +0800

    Update setup.sh

--- oci/setup.sh
+++ oci/setup.sh
@@ -1,5 +1,5 @@
 common() {
-  sudo pass $USER # interactive
+  sudo passwd $USER # interactive
   curl https://raw.githubusercontent.com/davidkhala/centos-collection/main/network/firewall.sh | bash -s allow-all
 }
 special(){
