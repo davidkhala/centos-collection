9d7b7fcd122c50c58189f8376814db2860f144e5
David Liu <david-khala@hotmail.com>
2024-05-09 16:56:10 +0800

    Update setup.sh

--- oci/setup.sh
+++ oci/setup.sh
@@ -1,6 +1,6 @@
 common() {
   curl https://raw.githubusercontent.com/davidkhala/centos-collection/main/network/firewall.sh | bash -s allow-all
-  echo -e "$1" | sudo passwd $USER
+  echo -e "$1\n$1\n" | sudo passwd $USER
 }
 special(){
   # only required for centos:stream
