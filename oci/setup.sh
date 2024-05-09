302afbfb24f1a252e7680607230c7aa54f4ad083
David Liu <david-khala@hotmail.com>
2024-05-09 15:18:04 +0800

    Update setup.sh

--- oci/setup.sh
+++ oci/setup.sh
@@ -1,1 +1,10 @@
-sudo dnf install python3-librepo -y
+common() {
+  sudo pass $USER # interactive
+  curl https://raw.githubusercontent.com/davidkhala/centos-collection/main/network/firewall.sh | bash -s allow-all
+}
+special(){
+  # only required for centos:stream
+  sudo dnf install python3-librepo -y
+}
+$@
+
