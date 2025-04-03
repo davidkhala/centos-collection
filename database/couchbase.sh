76d90256465768682f5b5e4b7643208786fe26d3
David Liu <david-khala@hotmail.com>
2025-04-03 15:58:44 +0800

    Update couchbase.sh

--- database/couchbase.sh
+++ database/couchbase.sh
@@ -3,6 +3,8 @@ install-edge-server(){
   wget https://packages.couchbase.com/releases/couchbase-edge-server/1.0.0/couchbase-edge-server-1.0.0.x86_64.rpm
   sudo dnf install -y couchbase-edge-server-1.0.0.x86_64.rpm
   rm couchbase-edge-server-1.0.0.x86_64.rpm
+  /opt/couchbase-edge-server/bin/couchbase-edge-server --version
 }
+
 $@
 
