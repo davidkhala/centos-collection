446a8c6823a23064044da4b15ff546922db350a9
David Liu <david-khala@hotmail.com>
2025-06-08 22:46:15 +0800

    add confluent platform install

---
+++ mq/confluent/README.md
@@ -0,0 +1,4 @@
+
+# Roadmap
+> After Confluent Platform 8.0, the `librdkafka`, `Avro`, and `libserdes` C/C++ client packages will NOT be available from the https://packages.confluent.io/rpm location.
+> You will need to obtain those client packages from https://packages.confluent.io/clients in future
---
+++ mq/confluent/platform.sh
@@ -0,0 +1,20 @@
+set -e
+register() {
+    # https://docs.confluent.io/platform/current/installation/installing_cp/rhel-centos.html#get-the-software
+    sudo yum install -y curl which
+    sudo rpm --import https://packages.confluent.io/rpm/7.9/archive.key
+    wget -P /etc/yum.repos.d/ https://raw.githubusercontent.com/davidkhala/centos-collection/refs/heads/main/mq/confluent/confluent.repo
+    sudo yum clean all
+
+}
+install() {
+    local community_edition=$1
+
+    if [ -z "$community_edition" ]; then
+        sudo yum install -y confluent-platform confluent-security
+    else
+        sudo yum install -y confluent-community-2.13
+    fi
+}
+
+$@
